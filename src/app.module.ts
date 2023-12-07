516e313462e7a70061b37a581dfe96969b28d7d2
Francisco1asir <fparnav1908@g.educaand.es>
2023-12-07 20:09:00 +0100

    1era tabla

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,26 @@ import { ClientesModule } from './modules/clientes/clientes.module';
 import { CategoriasModule } from './modules/categorias/categorias.module';
 import { UsuariosModule } from './modules/usuarios/usuarios.module';
 import { ProveedoresModule } from './modules/proveedores/proveedores.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [MotosModule, ClientesModule, CategoriasModule, UsuariosModule, ProveedoresModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: '192.168.100.243',
+      port: 5438,
+      database: 'Kawafrantri',
+      username: 'kawa',
+      password: 'kawa',
+      autoLoadEntities: true,
+      synchronize: true
+    }),
+    MotosModule, 
+    ClientesModule, 
+    CategoriasModule, 
+    UsuariosModule, 
+    ProveedoresModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/modules/proveedores/entities/proveedore.entity.ts
+++ src/modules/proveedores/entities/proveedore.entity.ts
@@ -1,1 +1,28 @@
-export class Proveedore {}
+import { Column, Entity, PrimaryColumn } from "typeorm";
+
+@Entity()
+export class Proveedore {
+    @PrimaryColumn('text',{
+        nullable: false,
+    }) 
+    cif: string;
+
+    @Column('text',{
+        unique: true,
+        nullable: true,
+    })
+    nombre: string;
+
+    @Column('text',{
+        unique: true,
+        nullable: true,
+    })
+    localidad: string;
+
+    @Column({
+        unique: true,
+        nullable: true,
+    })
+    telefono: number;
+
+}
