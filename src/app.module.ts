1fa00805f56d9ca416deede49d34e0fac787f140
Francisco1asir <fparnav1908@g.educaand.es>
2023-12-07 19:47:02 +0100

    entidades creadas

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MotosModule } from './modules/motos/motos.module';
+import { ClientesModule } from './modules/clientes/clientes.module';
+import { CategoriasModule } from './modules/categorias/categorias.module';
+import { UsuariosModule } from './modules/usuarios/usuarios.module';
+import { ProveedoresModule } from './modules/proveedores/proveedores.module';
 
 @Module({
-  imports: [],
+  imports: [MotosModule, ClientesModule, CategoriasModule, UsuariosModule, ProveedoresModule],
   controllers: [AppController],
   providers: [AppService],
 })
